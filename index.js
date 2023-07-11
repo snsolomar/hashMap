1894d49abe2c903942418ec83bd423ccb7e22ba6
Simon Solomon <snsolomar@gmail.com>
2023-07-11 12:29:47 -0500

    initialize

---
+++ index.js
@@ -0,0 +1,12 @@
+// Declaring a Hashmap using the Javascript Map() constructor
+let firstHashMap = new Map();
+
+// Declaring and initizialing a new hashmap constructor
+let secondHashMap = new Map([
+    [1, "first"],
+    [2, "second"],
+    [5, "fifth"]
+]);
+
+console.log("firstHashMap: ", firstHashMap);
+console.log("secondHashMap: ", secondHashMap);
